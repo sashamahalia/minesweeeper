5b5fea13e9072e210138fd2ead02664a8e8aa279
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-08-08 09:38:56 -0700

    Move game over message to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import Header from "./components/Header";
 import BombGrid from "./components/BombGrid";
+import { useSelector } from "react-redux";
+
 
 function App() {
-  return (
-    <div className="App">
-      
+
+  const { gameOver } = useSelector(state => state.mines);
+  const { turnCount } = useSelector(state => state.count);
+
+  const gameOverMessage = (
+      <>
+        <h1>GAME OVER</h1>
+        <h2>{`Turns played: ${turnCount}`}</h2>
+      </>
+    )
+  const gamePlay = (
+    <>
       <Header />
       <BombGrid />
+    </>
+  )
+  return (
+    <div className="App">
+      {gameOver ? gameOverMessage : gamePlay}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,10 @@
 import FlagCount from "./FlagCount";
 import TurnCount from "./TurnCount";
-import { useSelector } from "react-redux";
-
 function Header() {
-
-  const { gameOver } = useSelector(state => state.mines);
-
   return (
     <section>
       <FlagCount />
       <TurnCount />
-      <h2>{gameOver && "Game over"}</h2>
     </section>
   );
 }
