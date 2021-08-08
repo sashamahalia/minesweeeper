ebdaa74bb9be23871f61fc3635b17f070829f420
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-08-08 10:36:12 -0700

    Rewrite header to display game over message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import Header from "./components/Header";
 import BombGrid from "./components/BombGrid";
-import { useSelector } from "react-redux";
+
 
 
 function App() {
 
-  const { gameOver } = useSelector(state => state.mines);
-  const { turnCount } = useSelector(state => state.count);
-
-  const gameOverMessage = (
-      <>
-        <h1>GAME OVER</h1>
-        <h2>{`Turns played: ${turnCount}`}</h2>
-      </>
-    )
-  const gamePlay = (
-    <>
-      <Header />
-      <BombGrid />
-    </>
-  )
   return (
     <div className="App">
-      {gameOver ? gameOverMessage : gamePlay}
+      <Header />
+      <BombGrid />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
 import FlagCount from "./FlagCount";
 import TurnCount from "./TurnCount";
+import { useSelector } from "react-redux";
+
 function Header() {
+  const { gameOver } = useSelector(state => state.mines);
+  const { turnCount } = useSelector(state => state.count);
+
+  const gameOverMessage = (
+    <>
+      <h1>GAME OVER</h1>
+      <h2>{`Turns played: ${turnCount}`}</h2>
+    </>
+  )
+
+  const inPlay = (
+    <>
+    <FlagCount />
+    <TurnCount />
+    </>
+  )
+
   return (
     <section>
-      <FlagCount />
-      <TurnCount />
+      {gameOver ? gameOverMessage : inPlay}
     </section>
   );
 }
